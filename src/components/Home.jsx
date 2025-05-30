27b00f4368dbf7594853865eb8b7ce7dd7b3f14f
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-05-30 23:54:50 +0100

    Change contract address and ABI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
 import { timeDiff } from "../../utils/timeCalculation";
 
 const Home = (props) => {
 
   const { contract, signer } = props;
+  const [lastWithdrawal, setLastWithdrawal] = useState("")
 
   const handleClick = async () => {
     console.log(contract)
     const cont = Number(await contract.lastWithdrawal());
 
-    console.log(timeDiff(Date.now(), (cont * 1000)))
+    const time = (timeDiff(Date.now(), (cont * 1000)))
+    setLastWithdrawal(time);
+
+    
+
   }
 
   return (
     <>
       <h3>Home</h3>
       <button onClick={handleClick}>Logger</button>
+      {lastWithdrawal && <p>The last withdrawal was: {lastWithdrawal} </p>}
+      {!lastWithdrawal && <p>Loading...</p>}
     </>
   );
 }
